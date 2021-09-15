8cda3760a4b1cdcbcea0e5f857a2b028a937ff5a
Georgi Kamacharov <gkamacharov@gmail.com>
2021-09-15 13:35:35 -0400

    update to cleanup

--- kamafi.liability.core/Program.cs
+++ kamafi.liability.core/Program.cs
@@ -13,7 +13,9 @@ namespace kamafi.liability.core
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args)
+                .Build()
+                .Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
